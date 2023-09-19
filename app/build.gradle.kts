3034968cf782f94737d479c2f4bf5a7b0e374da5
CarlaPinton <carlapinton97@gmail.com>
2023-09-19 03:24:17 -0300

    Test
    Agregado de UnitTest y ActivityTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,15 @@ plugins {
 android {
     namespace = "com.curso.android.app.trabajofinal.trabajofinal"
     compileSdk = 33
-
+    packaging {
+        resources.excludes.addAll(
+            listOf(
+                "META-INF/LICENSE.md",
+                "META-INF/LICENSE-notice.md",
+                // other conflicting META-INF bits
+            )
+        )
+    }
     defaultConfig {
         applicationId = "com.curso.android.app.trabajofinal.trabajofinal"
         minSdk = 21
@@ -49,7 +57,10 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
